67c15a9bc6fdb85f263c153483ec61b5b4e4cca5
David Marshall <davidscottmarshall@gmail.com>
2025-08-13 16:28:29 -0400

    forms and Fletcherverse improvements
    Contact form: tweaked spam protection
    Fletcherverse comic: uploaded stage one color pages

--- Web Root/contact/contact-inky-stories.php
+++ Web Root/contact/contact-inky-stories.php
@@ -1,8 +1,32 @@
 <?php
-  // The global $_POST variable allows you to access the data sent with the POST method by name
-  // To access the data sent with the GET method, you can use $_GET
-  $say = htmlspecialchars($_POST['say']);
-  $to  = htmlspecialchars($_POST['to']);
-
-  echo  $say, ' ', $to;
+	$curl = curl_init();
+	$secret_key = "sk_26055848-9aae-4ee2-90d8-eb263c7bd09c";
+	$solution = "..."; // Extract the solution from the request body (_botpoison)
+	$data = array("secretKey" => $secret_key, "solution" => $solution);
+	$data_string = json_encode($data);
+	curl_setopt_array($curl, [
+		CURLOPT_URL => "https://api.botpoison.com/verify",
+		CURLOPT_CUSTOMREQUEST => "POST",
+		CURLOPT_POSTFIELDS => $data_string,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_HTTPHEADER => array('Content-Type: application/json', 'Content-Length: ' . strlen($data_string))
+	]);
+	$response = curl_exec($curl);
+	$error = curl_error($curl);
+	
+	curl_close($curl);
+	
+	if ($error) {
+		echo "cURL Error #:" . $error;
+	} else {
+		if (json_decode($response)->ok) {
+			echo "Not spam: allow request";
+		} else {
+			echo "Spam: reject request";
+		}
+	}  
 ?>
+
+
+
+
